22065a329a71dcdf78438a7a41fd720062ddb7ea
master <starodubcev@gmail.com>
2026-02-14 00:40:24 +0000

    feat: redirect /tags/X routes to /page/X in Logseq

--- logseq/custom.js
+++ logseq/custom.js
@@ -1,15 +1,28 @@
 // change theme to black
-if (document.querySelector("html").getAttribute("data-theme") === "light") {
-  document.querySelector("html").setAttribute("data-theme", "dark");
+if (
+  document
+    .querySelector("html")
+    .getAttribute("data-theme") === "light"
+) {
+  document
+    .querySelector("html")
+    .setAttribute("data-theme", "dark");
 
-  document.querySelector("body").classList.remove("light-theme");
-  document.querySelector("body").classList.remove("white-theme");
-  document.querySelector("body").classList.add("dark-theme");
+  document
+    .querySelector("body")
+    .classList.remove("light-theme");
+  document
+    .querySelector("body")
+    .classList.remove("white-theme");
+  document
+    .querySelector("body")
+    .classList.add("dark-theme");
 
   localStorage.setItem("theme", '"dark"');
 }
 
-document.querySelector(".journals-nav .flex-1").innerHTML = "Blog";
+document.querySelector(".journals-nav .flex-1").innerHTML =
+  "Blog";
 
 // Override the pushState method for correct hashchange event (for analytics)
 // seems router issue
@@ -19,3 +32,15 @@ history.pushState = function (state, title, url) {
   originalPushState.apply(history, arguments);
   window.dispatchEvent(new Event("hashchange"));
 };
+
+// Redirect /tags/X to /page/X so tags navigate to the associated page
+function redirectTagsToPage() {
+  var hash = window.location.hash;
+  if (hash.startsWith("#/tags/")) {
+    var tag = hash.slice("#/tags/".length);
+    window.location.hash = "#/page/" + tag;
+  }
+}
+
+window.addEventListener("hashchange", redirectTagsToPage);
+redirectTagsToPage();
